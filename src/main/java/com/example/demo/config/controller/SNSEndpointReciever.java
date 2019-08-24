a5a87c40d7aef5a9106cf10524bb324c776a6689
Cesar <cesar.gomezvela@gmail.com>
2019-08-24 01:22:29 -0400

    update receiver

--- src/main/java/com/example/demo/config/controller/SNSEndpointReciever.java
+++ src/main/java/com/example/demo/config/controller/SNSEndpointReciever.java
@@ -8,28 +8,36 @@ import org.springframework.cloud.aws.messaging.endpoint.NotificationStatus;
 import org.springframework.cloud.aws.messaging.endpoint.annotation.NotificationMessageMapping;
 import org.springframework.cloud.aws.messaging.endpoint.annotation.NotificationSubscriptionMapping;
 import org.springframework.cloud.aws.messaging.endpoint.annotation.NotificationUnsubscribeConfirmationMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/sns/receive")
 public class SNSEndpointReciever {
 	private static final Logger logger = LoggerFactory.getLogger(SNSEndpointReciever.class);
 
-    @NotificationMessageMapping
-    public void receiveNotification(@NotificationMessage String message, @NotificationSubject String subject) {
-        logger.info("Received message: {}, having subject: {}", message, subject);
-    }
+	@NotificationSubscriptionMapping
+	public void handleSubscriptionMessage(NotificationStatus status) {
+		//We subscribe to start receive the message
+		logger.info("Entra: handleSubscriptionMessage");
+		System.out.println("Entra: handleSubscriptionMessage");
+		status.confirmSubscription();
+	}
 
-    @NotificationUnsubscribeConfirmationMapping
-    public void confirmSubscriptionMessage(NotificationStatus notificationStatus) {
-        logger.info("Unsubscribed from Topic");
-        notificationStatus.confirmSubscription();
-    }
+	@NotificationMessageMapping
+	public void handleNotificationMessage(@NotificationSubject String subject, @NotificationMessage String message) {
+		// ...
+		logger.info("Entra: handleNotificationMessage");
+		System.out.println("Entra: handleNotificationMessage");
+		System.out.println("Entra: "+subject+","+message);
+	}
 
-    @NotificationSubscriptionMapping
-    public void confirmUnsubscribeMessage(NotificationStatus notificationStatus) {
-        logger.info("Subscribed to Topic");
-        notificationStatus.confirmSubscription();
-    }
+	@NotificationUnsubscribeConfirmationMapping
+	public void handleUnsubscribeMessage(NotificationStatus status) {
+		//e.g. the client has been unsubscribed and we want to "re-subscribe"
+		logger.info("Entra: handleUnsubscribeMessage");
+		System.out.println("Entra: handleUnsubscribeMessage");
+		status.confirmSubscription();
+	}
 }
